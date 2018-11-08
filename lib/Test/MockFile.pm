b4eb6d124da126bc34947a936fa528e864a7a990
Todd Rinaldo <toddr@cpanel.net>
2018-11-08 14:13:32 -0600

    Grammify the synopsis

--- lib/Test/MockFile.pm
+++ lib/Test/MockFile.pm
@@ -33,7 +33,7 @@ use constant FOLLOW_LINK_MAX_DEPTH => 10;
 
 =head1 NAME
 
-Test::MockFile - Lets tests validate code which interacts with files without the file system ever being touched. 
+Test::MockFile - Lets tests validate code that interacts with files without the file system ever being touched.
 
 =head1 VERSION
 
