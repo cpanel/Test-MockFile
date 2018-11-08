ba1df23b1e6a657698db83317d294a096413392b
Todd Rinaldo <toddr@cpanel.net>
2018-11-08 12:29:10 -0600

    Bump to 0.011 for release to CPAN

--- lib/Test/MockFile.pm
+++ lib/Test/MockFile.pm
@@ -37,11 +37,11 @@ Test::MockFile - Lets tests validate code which interacts with files without the
 
 =head1 VERSION
 
-Version 0.010
+Version 0.011
 
 =cut
 
-our $VERSION = '0.010';
+our $VERSION = '0.011';
 
 our %files_being_mocked;
 
