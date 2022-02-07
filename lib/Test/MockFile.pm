ea0bb09a7796fcb07bd63117a5ee4280ec0a3df6
Sawyer X <xsawyerx@cpan.org>
2022-02-07 14:17:29 +0100

    Normalize forward slashes:
    When receiving multiple forward slashes, we normalize it to a single
    one.
    
    Resolves GH #90.

--- lib/Test/MockFile.pm
+++ lib/Test/MockFile.pm
@@ -147,11 +147,6 @@ Relative paths are not supported:
     $file = Test::MockFile->file( '/bar',        '...' ); # ok     - absolute path
     $file = Test::MockFile->file( 'bar', '...' );         # ok     - current dir
 
-And if you have multiple forward slashes, it will confess as well:
-
-    use Test::MockFile;
-    $file = Test::MockFile->file( '//bar', '...' );
-
 =cut
 
 our %authorized_strict_mode_packages;
@@ -390,11 +385,6 @@ sub symlink {
 
 sub _validate_path {
     my $path = shift;
-
-    # Multiple forward slashes
-    if ( $path =~ m[/{2,}] ) {
-        confess('Repeated forward slashes in path');
-    }
 
     # Reject the following:
     # ./ ../ /. /.. /./ /../
@@ -715,6 +705,10 @@ sub _abs_path_to_file {
     my ($path) = shift;
 
     defined $path or return;
+
+    # cleanup multiple slashes
+    $path =~ s{//+}{/}xmsg;
+
     return $path if $path =~ m{^/};
 
     return Cwd::getcwd() . "/$path";
