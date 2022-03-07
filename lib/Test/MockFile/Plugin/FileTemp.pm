44e012a7cf7f2c4ce2994c6fb470c8edd20eece0
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2022-03-07 14:54:58 -0700

    Fixup tempfile in scalar context
    Fix #173:
    
    Make sure we respect the call of File::Temp::tempfile
    in scalar context and create an unlinked file.

--- lib/Test/MockFile/Plugin/FileTemp.pm
+++ lib/Test/MockFile/Plugin/FileTemp.pm
@@ -35,7 +35,10 @@ sub register {
 
             Test::MockFile::add_strict_rule_for_filename( $out[1] => 1 );
 
-            return wantarray ? @out : $out[0];
+            return @out if wantarray;
+
+            File::Temp::unlink0( $out[0], $out[1] );
+            return $out[0];
         }
     );
 
